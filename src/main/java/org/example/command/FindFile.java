d2ebeff0aa21dfc6ef2df5b605c97f662fb9bfc9
Uka0001 <ukraine0001@gmail.com>
2022-09-04 15:36:51 +0300

    find file final fix

--- src/main/java/org/example/command/FindFile.java
+++ src/main/java/org/example/command/FindFile.java
@@ -3,6 +3,17 @@ package org.example.command;
 import java.io.File;
 import java.util.List;
 
+//шукаєм файл
 public interface FindFile {
-    File findFile(List<String> args);
+
+    public default File findFile(List<String> args, Context context) {
+        File currentDirectory = context.getCurrentDirectory();
+        // перевіряємо чи існує аргумент та чи існує файл
+        if (args.isEmpty()) {
+            return null;
+        } else {
+            File file = new File(currentDirectory, args.get(0));
+            return file.exists() ? file : null;
+        }
+    }
 }
